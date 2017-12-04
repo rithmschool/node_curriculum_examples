03f0b1a76f49247d103cc2d29d42b7cfe8160e72
mhueter <mthueter@gmail.com>
2017-12-04 14:33:09 -0800

    cleaned up express forms

--- express_forms/app.js
+++ express_forms/app.js
@@ -1,30 +1,31 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.set("view engine", "pug");
-app.use(bodyParser.urlencoded({extended:true}));
+const app = express();
 
-app.get("/", function(req,res){
-  res.redirect('/form-with-get');
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+  return res.redirect('/form-with-get');
 });
 
-app.get("/form-with-get", function(req,res){
-  res.render("form-with-get");
+app.get('/form-with-get', function(req, res) {
+  return res.render('form-with-get');
 });
 
-app.get("/form-with-post", function(req,res){
-  res.render("form-with-post");
+app.get('/form-with-post', function(req, res) {
+  return res.render('form-with-post');
 });
 
-app.get("/submit-form-with-get", function(req,res){
-  res.send(req.query);
+app.get('/submit-form-with-get', function(req, res) {
+  return res.send(req.query);
 });
 
-app.post("/submit-form-with-post", function(req,res){
-  res.send(req.body);
+app.post('/submit-form-with-post', function(req, res) {
+  return res.send(req.body);
 });
 
-app.listen(3000, function(){
-  console.log("Server is listening on port 3000");
+app.listen(3000, function() {
+  console.log('Server is listening on port 3000');
 });
