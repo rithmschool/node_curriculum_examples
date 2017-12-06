a04dd32367d3762144eab1b75cd11a136a492c6f
mhueter <mthueter@gmail.com>
2017-12-05 20:53:42 -0800

    updated restful routing example

--- express_restful_routing/app.js
+++ express_restful_routing/app.js
@@ -1,57 +1,59 @@
-var express = require("express");
-var app = express();
-var methodOverride = require("method-override");
-var bodyParser = require("body-parser");
-var fs = require("fs");
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 var id = 1;
-var instructors = []
+const instructors = [];
 
-app.set("view engine", "pug");
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-app.get('/', function(req,res){
-    res.redirect('/instructors')
+app.get('/', (req, res) => {
+  return res.redirect('/instructors');
 });
 
-app.get("/instructors", function(req,res){
-    res.render('index', {instructors})
+app.get('/instructors', (req, res) => {
+  return res.render('index', { instructors });
 });
 
-app.get("/instructors/new", function(req,res){
-    res.render("new");
+app.get('/instructors/new', (req, res) => {
+  return res.render('new');
 });
 
-app.get("/instructors/:id", function(req,res){
-    var instructor = instructors.find(v => v.id === Number(req.params.id))
-    res.render('show', {instructor})
+app.get('/instructors/:id', (req, res) => {
+  const instructor = instructors.find(v => v.id === Number(req.params.id));
+  return res.render('show', { instructor });
 });
 
-app.get("/instructors/:id/edit", function(req,res){
-    var instructor = instructors.find(v => v.id === Number(req.params.id))
-    res.render('edit', {instructor})
+app.get('/instructors/:id/edit', (req, res) => {
+  const instructor = instructors.find(v => v.id === Number(req.params.id));
+  return res.render('edit', { instructor });
 });
 
-app.post("/instructors", function(req,res){
-    instructors.push(Object.assign({}, req.body, {id}))
-    id++
-    res.redirect('/instructors')
+app.post('/instructors', (req, res) => {
+  instructors.push(Object.assign({}, req.body, { id }));
+  id++;
+  return res.redirect('/instructors');
 });
 
-app.patch("/instructors/:id", function(req,res){
-    var instructor = instructors.find(v => v.id === Number(req.params.id))
-    instructor.name = req.body.name
-    res.redirect('/instructors')
+app.patch('/instructors/:id', (req, res) => {
+  const instructor = instructors.find(v => v.id === Number(req.params.id));
+  instructor.name = req.body.name;
+  return res.redirect('/instructors');
 });
 
-app.delete("/instructors/:id", function(req,res){
-    var instructorIdx = instructors.findIndex(v => v.id === Number(req.params.id))
-    instructors.splice(instructorIdx,1)
-    res.redirect('/instructors')
+app.delete('/instructors/:id', (req, res) => {
+  const instructorIdx = instructors.findIndex(
+    v => v.id === Number(req.params.id)
+  );
+  instructors.splice(instructorIdx, 1);
+  return res.redirect('/instructors');
 });
 
-app.listen(3000, function(){
-  console.log("Server is listening on port 3000");
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
