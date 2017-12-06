f5a5dd6032be5cf93f3715476c9daed3ddc19674
mhueter <mthueter@gmail.com>
2017-12-05 22:48:51 -0800

    added editor config and better syntax for express router exercise

--- express_router/app.js
+++ express_router/app.js
@@ -1,23 +1,24 @@
-var express = require("express");
-var app = express();
-var methodOverride = require("method-override");
-var bodyParser = require("body-parser");
-var morgan = require("morgan");
-var fs = require("fs");
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
 
-app.set("view engine", "pug");
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
-app.use(morgan("tiny"));
+const app = express();
 
-var instructorRoutes = require("./routes/instructors")
-app.use('/instructors', instructorRoutes)
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+app.use(morgan('tiny'));
 
-app.get('/', function(req,res){
-    res.redirect('/instructors')
+const instructorRoutes = require('./routes');
+app.use('/instructors', instructorRoutes);
+
+app.get('/', (req, res) => {
+  return res.redirect('/instructors');
 });
 
-app.listen(3000, function(){
-  console.log("Server is listening on port 3000");
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
---
+++ express_router/routes/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const router = express.Router();
+
+var id = 1;
+const instructors = [];
+
+router
+  .route('')
+  .get((req, res) => {
+    return res.render('index', { instructors });
+  })
+  .post((req, res) => {
+    instructors.push(Object.assign({}, req.body, { id }));
+    id++;
+    return res.redirect('/instructors');
+  });
+
+router
+  .route('/new')
+  .get((req, res) => {
+    return res.render('new');
+  });
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    const instructor = instructors.find(v => v.id === Number(req.params.id));
+    return res.render('show', { instructor });
+  })
+  .patch((req, res) => {
+    const instructor = instructors.find(v => v.id === Number(req.params.id));
+    instructor.name = req.body.name;
+    return res.redirect('/instructors');
+  })
+  .delete((req, res) => {
+    const instructorIdx = instructors.findIndex(
+      v => v.id === Number(req.params.id)
+    );
+    instructors.splice(instructorIdx, 1);
+    return res.redirect('/instructors');
+  });
+
+router
+  .route('/:id/edit')
+  .get((req, res) => {
+    const instructor = instructors.find(v => v.id === Number(req.params.id));
+    return res.render('edit', { instructor });
+  });
+
+module.exports = router;
