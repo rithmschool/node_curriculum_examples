a85244473e26473c8e4489fcf4fdda9c587d4153
Joel Burton <joel@joelburton.com>
2018-03-27 08:06:47 -0700

    Fix inconsistent startup port # message

--- mongoose_intro/app.js
+++ mongoose_intro/app.js
@@ -35,5 +35,5 @@ app.get('/', (req, res) => {
 
 // server init
 app.listen(5000, () => {
-  console.log('Server is listening on port 3000');
+  console.log('Server is listening on port 5000');
 });
